c5db63485af23a489bc043c3a85ae6ace240904b
haleyashleypraesent <haleyashleypraesent@users.noreply.github.com>
2018-08-14 20:09:40 +1000

    Made some start on rewritting some code.

--- src/main/java/weather2/weathersystem/storm/WeatherTypes.java
+++ src/main/java/weather2/weathersystem/storm/WeatherTypes.java
@@ -14,12 +14,37 @@ public class WeatherTypes {
 	public static void initWeatherTypes() {
     	weatherEntTypes = new ArrayList();
         WeatherEntityConfig sConf = new WeatherEntityConfig();
+		
+		/* will be rewritten to
+		0 = Gustnado
+		1 = EF0
+		2 = EF1
+		3 = EF2
+		4 = EF3
+		5 = EF4
+		6 = EF5
+		7 = TCyclone - Tropical Depression SSHWS
+		8 = TCyclone - Tropical Storm SSHWS
+		9 = TCyclone - Category 1 SSHWS
+		A = TCyclone - Category 2 SSHWS
+		B = TCyclone - Category 3 Major SSHWS
+		C = TCyclone - Category 4 Major SSHWS
+		D = TCyclone - Category 5 Super SSWHS
+		*/
+		
+		//Gustnado
+		sConf.tornadoInitialSpeed = 0.1F;
+		
+	// OLD	past this point
+		
         //0 = spout
         //1 = F1
         //2 = F3
         //3 = F5
         //4 = F6
         //5 = Hurricane C1
+		
+		
         //water spout
         sConf.tornadoInitialSpeed = 0.2F;
         sConf.tornadoPullRate = 0.04F;
